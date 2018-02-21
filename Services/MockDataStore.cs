b21d75f6b0f453338abd88579ea7af42e627c822
spaceliner <sund14@myunitec.ac.nz>
2018-02-22 11:13:52 +1300

    Initial commit

---
+++ Services/MockDataStore.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Wallet.Models;
+
+[assembly: Xamarin.Forms.Dependency(typeof(Wallet.Services.MockDataStore))]
+namespace Wallet.Services
+{
+    public class MockDataStore : IDataStore<Item>
+    {
+        List<Item> items;
+
+        public MockDataStore()
+        {
+            items = new List<Item>();
+            var mockItems = new List<Item>
+            {
+                new Item { Id = Guid.NewGuid().ToString(), Text = "CreateAccount", Description="Create a new account" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "ExchangeRate", Description="Find your ExchangeRate Here" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "MyWallet", Description="This is your wallet infor" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Transaction", Description="Pay someone?" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Information", Description="More information" },
+                
+            };
+
+            foreach (var item in mockItems)
+            {
+                items.Add(item);
+            }
+        }
+
+        public async Task<bool> AddItemAsync(Item item)
+        {
+            items.Add(item);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> UpdateItemAsync(Item item)
+        {
+            var _item = items.Where((Item arg) => arg.Id == item.Id).FirstOrDefault();
+            items.Remove(_item);
+            items.Add(item);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteItemAsync(Item item)
+        {
+            var _item = items.Where((Item arg) => arg.Id == item.Id).FirstOrDefault();
+            items.Remove(_item);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<Item> GetItemAsync(string id)
+        {
+            return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(items);
+        }
+    }
+}
---
+++ Views/Exchange.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Wallet.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Exchange : ContentPage
+	{
+        String currency= "";
+		public Exchange ()
+		{
+			InitializeComponent ();
+		}
+        private void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                currency = (string)picker.ItemsSource[selectedIndex];
+            }
+        }
+
+        private void Trans_ButtonClicked(object sender, EventArgs e)
+        {
+            int inputnumber = Convert.ToInt32(input.Text);
+
+
+            string url = "https://blockchain.info/tobtc?currency=" + currency + "&value=" + inputnumber;
+
+            output.Text = HttpGet(url);
+
+
+        }
+
+        ///?<summary>
+        ///?后台发送GET请求
+        ///?</summary>
+        ///?<param?name="url">服务器地址</param>
+        ///?<param?name="data">发送的数据</param>
+        ///?<returns></returns>
+        public string HttpGet(string url)
+        {
+            try
+            {
+                //创建Get请求
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.ContentType = "text/html;charset=UTF-8";
+
+                //接受返回来的数据
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader streamReader = new StreamReader(stream, Encoding.GetEncoding("utf-8"));
+                string retString = streamReader.ReadToEnd();
+
+                streamReader.Close();
+                stream.Close();
+                response.Close();
+
+                return retString;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+
+    }
+
+
+}
